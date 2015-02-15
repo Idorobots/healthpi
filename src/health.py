1a0e6f0c12955169c8497eb56e40f55a05df272e
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-02-15 16:10:45 +0100

    Added stub of the health monitoring server.

---
+++ src/health.py
@@ -0,0 +1,59 @@
+import json
+import os
+import re
+import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Health(BaseHTTPRequestHandler):
+    def response(self, code, body):
+        self.send_response(code)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        wrapped_body = {"status" : "ok",
+                        "result" : body}
+
+        if code // 100 != 2:
+            wrapped_body["status"] = "error"
+
+        self.wfile.write(bytes(json.dumps(wrapped_body), "utf-8"))
+
+    def do_GET(self):
+        endpoints = {"/temp/": self.get_temp,
+                     "/load/": self.get_load,
+                     "/memory/": self.get_memory,
+                     "/": lambda: {"endpoints": list(endpoints)}}
+
+        if self.path in endpoints:
+            return self.response(200, endpoints[self.path]())
+
+        else:
+            return self.response(404, "Not found")
+
+    def get_temp(self):
+        temp = open("/sys/class/thermal/thermal_zone0/temp", "r").readline()
+        return {"thermal_zone0": int(temp)/1000}
+
+    def get_load(self):
+        load = open("/proc/loadavg", "r").readline()
+        loads = re.split(" ", load)
+        return {"1min": float(loads[0]),
+                "5min": float(loads[1]),
+                "15min": float(loads[2])}
+
+    def get_memory(self):
+        memory = {}
+
+        for line in open("/proc/meminfo", "r"):
+            info = re.split("\s+", line)
+            stat = info[0]
+            stat = stat[0:len(stat)-1]
+            memory[stat] = 1024 * int(info[1])
+
+        return memory
+
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    server = HTTPServer(("localhost", port), Health)
+    print("Health server started on port ", port)
+    server.serve_forever()
