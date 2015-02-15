130c8ce0be523de969c8a0bc45ec9b325c700fee
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-02-15 18:33:08 +0100

    Added better connection error handling.

--- src/check.py
+++ src/check.py
@@ -8,32 +8,40 @@ import sys
 
 
 def check_health(url, optlist):
-    result = requests.get(url).json()
+    try:
+        result = requests.get(url).json()
 
-    if result["status"] == "error":
-        print(url, "check failed!")
-        return
+        if result["status"] == "error":
+            print(url, "check failed!")
+            return
 
-    print("#######", url, "#######")
+        print("#######", url, "#######")
 
-    endpoints = result["result"]["endpoints"]
-    allowed = "--endpoints" in optlist and re.split(",", optlist["--endpoints"]) or endpoints
+        endpoints = result["result"]["endpoints"]
+        allowed = "--endpoints" in optlist and re.split(",", optlist["--endpoints"]) or endpoints
 
-    for endpoint in endpoints:
-        if endpoint != "/" and endpoint in allowed:
-            check_endpoint(url, endpoint)
+        for endpoint in endpoints:
+            if endpoint != "/" and endpoint in allowed:
+                check_endpoint(url, endpoint)
+
+    except:
+        print(url, "unreachable!")
 
 def check_endpoint(url, endpoint):
-    result = requests.get(url + endpoint).json()
+    try:
+        result = requests.get(url + endpoint).json()
 
-    if result["status"] == "error":
-        print(endpoint, "check failed!")
+        if result["status"] == "error":
+            print(endpoint, "check failed!")
 
-    else:
-        print(endpoint, ":")
+        else:
+            print(endpoint, ":")
 
-        for stat in result["result"]:
-            print("\t", stat, ":", result["result"][stat])
+            for stat in result["result"]:
+                print("\t", stat, ":", result["result"][stat])
+
+    except:
+        print(endpoint, "unreachable!")
 
 if __name__ == "__main__":
     optlist, args = getopt.getopt(sys.argv[1:], "", ["endpoints="])
